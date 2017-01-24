47e0f9537ce58d3446798ab3a89ef30a90fce898
DARCY MCCABE <DARMCCA@GMAIL.COM>
2017-01-24 06:40:14 -0800

    Finished all the TODOs

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -3,7 +3,10 @@
 (function(module) {
   const aboutController = {};
 
-  // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
+  // TODO: DONE Define a function that hides all main section elements, and then reveals just the #about section:
+aboutController.index = ()
+$('#about').show().siblings().hide();
+
 
   module.aboutController = aboutController;
 })(window);
